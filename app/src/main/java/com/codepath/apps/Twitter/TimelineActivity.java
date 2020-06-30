3bf291469839a639d5e75c81da2ef0c06f1c2f2d
Annie Zhu <aczhu@fb.com>
2020-06-30 18:06:59 -0400

    pull to refresh

--- app/src/main/java/com/codepath/apps/Twitter/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/Twitter/TimelineActivity.java
@@ -13,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.codepath.apps.Twitter.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -31,6 +32,36 @@ public class TimelineActivity extends AppCompatActivity {
     RecyclerView rvt;
     List<Tweet> tweets;
     private final int REQUEST_CODE=30;
+    private SwipeRefreshLayout swipeContainer;
+    TweetAdapter adapter;
+    public static final String TAG="TimelineActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {fetchTimelineAsync(0);
+            }
+        });
+        swipeContainer.setColorSchemeResources(R.color.blue);
+
+        if (android.os.Build.VERSION.SDK_INT >= 21){
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.status));
+        }
+
+        client = TwitterApp.getRestClient(this);
+        rvt = findViewById(R.id.rv_tweets);
+        tweets= new ArrayList<>();
+        adapter=new TweetAdapter(this, tweets);
+        rvt.setLayoutManager(new LinearLayoutManager(this));
+        rvt.setAdapter(adapter);
+        populateHomeTimeline();
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -60,27 +91,30 @@ public class TimelineActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
+    public void fetchTimelineAsync(int page) {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                adapter.clear();
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    tweets.addAll(Tweet.fromJsonArray(jsonArray));
+                    Log.d(TAG, "tweets size : "+tweets.size());
+                    adapter.notifyDataSetChanged();
+                    Log.d(TAG, "gets JSON");
 
-    TweetAdapter adapter;
-    public static final String TAG="TimelineActivity";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timeline);
-        if (android.os.Build.VERSION.SDK_INT >= 21){
-            Window window = this.getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.setStatusBarColor(this.getResources().getColor(R.color.status));
-        }
+                } catch (JSONException e) {
+                    Log.d(TAG, "JSON Exception");
+                    e.printStackTrace();
+                }
+                swipeContainer.setRefreshing(false);
+            }
 
-        client = TwitterApp.getRestClient(this);
-        rvt = findViewById(R.id.rv_tweets);
-        tweets= new ArrayList<>();
-        adapter=new TweetAdapter(this, tweets);
-        rvt.setLayoutManager(new LinearLayoutManager(this));
-        rvt.setAdapter(adapter);
-        populateHomeTimeline();
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d("DEBUG", "Fetch timeline error");
+            }
+        });
     }
 
     private void populateHomeTimeline() {
--- app/src/main/java/com/codepath/apps/Twitter/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/Twitter/TweetAdapter.java
@@ -25,6 +25,16 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
         this.context = context;
         this.tweets = tweets;
     }
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Tweet> list) {
+        tweets.addAll(list);
+        notifyDataSetChanged();
+    }
 
     @NonNull
     @Override
